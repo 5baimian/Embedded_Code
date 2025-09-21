48a240690e9d1006d965b39ca2c0ab88df1277ae
xiaolong-51 <2416339565@qq.com>
2025-09-21 23:52:30 +0800

    整理f103文件--921

--- STM32/F103C8T6/4-1OLED显示屏/Hardware/LightSensor.h
+++
@@ -1,8 +0,0 @@
-#include "stm32f10x.h"                  // Device header
-
-#ifndef __LIGHTSENSOR_H
-#define __LIGHTSENSOR_H
-void LightSensor_Init(void);
-uint8_t LightSensor_Get(void);
-#endif
-
--- STM32/F103C8T6/5-1对射式传感器计次/Hardware/CountSeneor.h
+++
@@ -1,9 +0,0 @@
-#include "stm32f10x.h"                  // Device header
-
-#ifndef __COUNTSENSOR_H
-#define __COUNTSENSOR_H
-
-void CountSensor_Init(void);
-uint16_t CountSensor_Get(void);
-
-#endif
